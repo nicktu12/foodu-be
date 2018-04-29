7598628af0da435b31c595aeb5273f7e65d00395
Nick Teets <nickteets@gmail.com>
2018-04-28 20:34:31 -0600

    Set up test endpoint

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/api/v1/test', (request, response) => {
+    response.send('Foodu is the future');
+});
+
+app.listen(3001, () => {
+    console.log('Express app running on localhost:3001');
+});
